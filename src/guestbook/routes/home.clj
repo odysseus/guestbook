76cb8e2dbbc56ecbeec941c3c9dd065150b6fee1
Ryan Case <rmcase@gmail.com>
2015-03-25 13:05:58 -0600

    Added date formatting

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -4,53 +4,54 @@
             [hiccup.form :refer :all]
             [guestbook.models.db :as db]))
 
+(defn format-time [timestamp]
+  (-> "dd/MM/yyyy"
+      (java.text.SimpleDateFormat.)
+      (.format timestamp)))
+
 (defn show-guests []
   [:ul.guests
    (for [{:keys [message name timestamp]} (db/read-guests)]
      [:li
       [:blockquote message]
       [:p "-" [:cite name]]
-      [:time timestamp]])])
+      [:time (format-time timestamp)]])])
 
 (defn home [& [name message error]]
   (layout/common
-   [:h1 "Entries"]
-   [:em [:p "Welcome to the guestbook"]]
-   [:p error]
+    [:h1 "Entries"]
+    [:em [:p "Welcome to the guestbook"]]
+    [:p error]
 
-   ; Show the guests
-   (show-guests)
-   [:hr]
+    ; Show the guests
+    (show-guests)
+    [:hr]
 
-   ; Form to create a new message
-   (form-to [:post "/"]
-            [:p "Name:"]
-            (text-field "name" name)
-            [:p "Message:"]
-            (text-area {:rows 10 :cols 40} "message" message)
-            [:br]
-            (submit-button "comment"))))
+    ; Form to create a new message
+    (form-to [:post "/"]
+             [:p "Name:"]
+             (text-field "name" name)
+             [:p "Message:"]
+             (text-area {:rows 10 :cols 40} "message" message)
+             [:br]
+             (submit-button "comment"))))
 
-(defn format-time [timestamp]
-  (-> "dd/MM/yyyy"
-      (java.text.SimpleDateFormat.)
-      (.format timestamp)))
 
 (defn save-message [name message]
   (cond
-   (and (empty? name) (empty? message))
-   (home name message "No data entered")
+    (and (empty? name) (empty? message))
+    (home name message "No data entered")
 
-   (empty? name)
-   (home name message "Name field has been let blank")
+    (empty? name)
+    (home name message "Name field has been let blank")
 
-   (empty? message)
-   (home name message "Message field has been let blank")
+    (empty? message)
+    (home name message "Message field has been let blank")
 
-   :else
-   (do
-     (db/save-message name message)
-     (home))))
+    :else
+    (do
+      (db/save-message name message)
+      (home))))
 
 (defroutes home-routes
   (GET "/" [] (home))
